f119f56a98039de61d5f7dd39935ad32010d2aed
Joppert123 <44192052+Joppert123@users.noreply.github.com>
2020-08-16 16:59:36 +0200

    PF plaatje toegevoegd

--- app/src/main/java/com/example/anyapp/Config.java
+++ app/src/main/java/com/example/anyapp/Config.java
@@ -2,7 +2,9 @@ package com.example.anyapp;
 
 public class Config {
 
-    private static final String URL = "http://192.168.178.17/AnyApp/Android/V1/";
+//    private static final String URL = "http://192.168.178.17/AnyApp/Android/V1/";
+    private static final String URL = "http://192.168.178.32/AnyApp/Android/V1/";
+
 
     public static final String URL_REGISTER = URL+"registerUser.php";
     public static final String URL_LOGIN = URL+"loginUser.php";
